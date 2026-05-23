10b978a6baab1f435a8ac9736dfb59c3320f0c4b
Judson Daniels <judsodan@amazon.com>
2026-05-23 12:35:35 -0500

    Study cards: corner triangle number, larger team logo, smaller manufacturer logo, text labels

--- game.js
+++ game.js
@@ -157,10 +157,14 @@ function showStudyMode() {
         const card = document.createElement("div");
         card.className = "study-card";
         card.innerHTML = `
-            <img src="img/cars/car_${driver.number}.png" alt="Car #${driver.number}" class="study-car-img" />
+            <div class="study-car-container">
+                <div class="study-corner-number">${driver.number}</div>
+                <img src="img/cars/car_${driver.number}.png" alt="Car #${driver.number}" class="study-car-img" />
+            </div>
             <div class="study-info">
-                <div class="study-number">#${driver.number}</div>
                 <div class="study-name">${driver.name}</div>
+                <div class="study-team-text">${driver.team}</div>
+                <div class="study-engine-text">${driver.engine}</div>
                 <div class="study-logos">
                     <img src="${manufacturerLogos[driver.engine]}" alt="${driver.engine}" class="study-manufacturer-logo" />
                     <img src="${teamLogos[driver.team]}" alt="${driver.team}" class="study-team-logo" />
